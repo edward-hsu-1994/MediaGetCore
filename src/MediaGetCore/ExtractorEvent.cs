f726fdee2a2bc7a8c1d2c82714c2de48dd51210b
徐培堯 <st89382000@gmail.com>
2017-10-28 23:50:39 +0800

    清除專案

--- src/MediaGetCore/ExtractorEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MediaGetCore{
-    public delegate void ProcessEvent(IExtractor sender,double percent);
-    public delegate void CompletedEvent(IExtractor sender, MediaInfo[] result);
-}
